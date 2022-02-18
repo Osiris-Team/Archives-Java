f29876a89d65a87d744d8f9a14151c05c8516113
Osiris Team <osiris_support@pm.me>
2022-02-18 20:30:05 +0100

    added tests

---
+++ src/test/java/com/osiris/archivesjava/TarXzTest.java
@@ -0,0 +1,30 @@
+package com.osiris.archivesjava;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TarXzTest {
+    @Test
+    void test() throws IOException {
+        File f1 = new File(System.getProperty("user.dir")+"/src/test/f1.txt");
+        File f2 = new File(System.getProperty("user.dir")+"/src/test/f2.txt");
+        File f3 = new File(System.getProperty("user.dir")+"/src/test/folder/f3.txt");
+        File f4 = new File(System.getProperty("user.dir")+"/src/test/folder/f4.txt");
+        f4.getParentFile().mkdirs();
+        f1.createNewFile();
+        Files.write(f1.toPath(), "File nr1".getBytes(StandardCharsets.UTF_8));
+        f2.createNewFile();
+        Files.write(f2.toPath(), "File nr2".getBytes(StandardCharsets.UTF_8));
+        f3.createNewFile();
+        Files.write(f3.toPath(), "File nr3".getBytes(StandardCharsets.UTF_8));
+        f4.createNewFile();
+        Files.write(f4.toPath(), "File nr4".getBytes(StandardCharsets.UTF_8));
+        new TarXz("test").addFile(f1, f2, f3, f4).createInDir(System.getProperty("user.dir")+"/src/test");
+    }
+}
